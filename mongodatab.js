b714b9908115807e17854feb63a2a98115c42f52
Okekunle Oluwatobiloba <okekunletobi@gmail.com>
2020-03-30 22:02:34 +0100

    Update

--- mongodatab.js
+++ mongodatab.js
@@ -3,7 +3,7 @@ const mongo = require('mongodb');
 let MongoClient = require("mongodb").MongoClient;
 let url = "mongodb://localhost:27017/oluwatobilobaoke";
 
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
     if (err) throw err;
     console.log("Database created by oluwatobilobaoke");
     db.close();
